559c51a369dc62c0805026a5d7eca92671a6267b
Gabriel Oliveira Gomes <00001090692699sp@aluno.educacao.sp.gov.br>
2023-10-27 11:33:37 -0300

    saving chat header made

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,16 +1,50 @@
-import styled from "styled-components"
+import { useRouter } from "next/router";
+import { auth } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import styled from "styled-components";
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert } from "@material-ui/icons";
 
-function ChatScreen() {
+function ChatScreen({ chat, messages }) {
+  const [user] = useAuthState(auth);
+
+  const router = useRouter();
+
   return (
     <Container>
+      <Header>
+        <Avatar />
 
+        <HeaderInformation>
+          <h3>Email do recipiente</h3>
+          <p>Ultima vez visto...</p>
+        </HeaderInformation>
+        <HeaderIcons>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </HeaderIcons>
+      </Header>
     </Container>
-  )
+  );
 }
 
-const Container = styled.div`
+export default ChatScreen;
 
-`
-
-
-export default ChatScreen
+const Container = styled.div``;
+const Header = styled.div`
+  position: sticky;
+  background-color: white;
+  z-index: 100;
+  top: 0;
+  display: flex;
+  padding: 11px;
+  height: 80px;
+  align-items: center;
+  border-bottom: 1px solid whitesmoke;
+`;
+const HeaderInformation = styled.div``;
+const HeaderIcons = styled.div``;
--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -1,72 +1,73 @@
-import Head from "next/head"
-import styled from "styled-components"
-import Sidebar from './../../components/Sidebar';
+import Head from "next/head";
+import styled from "styled-components";
+import Sidebar from "./../../components/Sidebar";
 import ChatScreen from "@/components/ChatScreen";
-import { db } from "../../firebase";
+import { auth, db } from "../../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import getRecipientEmail from "utils/getRecipientEmail";
 
 function Chat({ chat, messages }) {
-    return (
-        <Container>
-            <Head>
-                <title>
-                    Chat
-                </title>
-            </Head>
-            <Sidebar />
-            <ChatContainer>
-                <ChatScreen />
-            </ChatContainer>
-        </Container>
-    )
+  const [user] = useAuthState(auth);
+
+  return (
+    <Container>
+      <Head>
+        <title>Conversa com {getRecipientEmail(chat.users, user)}</title>
+      </Head>
+      <Sidebar />
+      <ChatContainer>
+        <ChatScreen chat={chat} messages={messages} />
+      </ChatContainer>
+    </Container>
+  );
 }
+export default Chat;
 
 export async function getServerSideProps(context) {
-    const ref = db.collection("chats").doc(context.query.id);
+  const ref = db.collection("chats").doc(context.query.id);
 
-    const messagesRes = await ref.collection("messages").orderBy("timestamp", "asc").get();
+  const messagesRes = await ref
+    .collection("messages")
+    .orderBy("timestamp", "asc")
+    .get();
 
-    const messages = messagesRes.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-    })).map(messages => ({
-        ...messages,
-        timestamp: messages.timestamp.toDate().getTime()
+  const messages = messagesRes.docs
+    .map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
     }))
+    .map((messages) => ({
+      ...messages,
+      timestamp: messages.timestamp.toDate().getTime(),
+    }));
 
-    const chatRes = await ref.get();
-    const chat = {
-        id: chatRes.id,
-        ...chatRes.data()
-    }
+  const chatRes = await ref.get();
+  const chat = {
+    id: chatRes.id,
+    ...chatRes.data(),
+  };
 
-    console.log(chat, messages)
-
-    return {
-        props: {
-            messages: JSON.stringify(messages)
-        }
-    }
-
+  return {
+    props: {
+      messages: JSON.stringify(messages),
+      chat: chat,
+    },
+  };
 }
 
 const Container = styled.div`
-    display: flex;
-
-`
+  display: flex;
+`;
 
 const ChatContainer = styled.div`
-    flex: 1;
-    overflow: scroll;
-    height: 100vh;
+  flex: 1;
+  overflow: scroll;
+  height: 100vh;
 
-    &::-webkit-scrollbar{
-        display: none;
-    }
+  &::-webkit-scrollbar {
+    display: none;
+  }
 
-    -ms-overflow-style: none;
-    scrollbar-width: none;
-
-`
-
-
-export default Chat
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+`;
