20fb7e19e47915d1c5fc143393323bb1aff277d9
Gabriel Oliveira Gomes <00001090692699sp@aluno.educacao.sp.gov.br>
2023-10-27 11:49:40 -0300

    saving, pfp sizes changd

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,100 +1,104 @@
-import { Avatar, IconButton, Button } from "@material-ui/core"
-import styled from 'styled-components';
-import ChatIcon from "@material-ui/icons/Chat"
-import MoreVertIcon from "@material-ui/icons/MoreVert"
-import SearchIcon from "@material-ui/icons/Search"
-import * as EmailValidator from 'email-validator'
+import { Avatar, IconButton, Button } from "@material-ui/core";
+import styled from "styled-components";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
+import * as EmailValidator from "email-validator";
 import { auth, db } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useCollection } from "react-firebase-hooks/firestore"
+import { useCollection } from "react-firebase-hooks/firestore";
 import Chat from "./Chat";
 
 export default function Sidebar() {
-
-  const [user] = useAuthState(auth)
-  const userChatRef = db.collection('chats').where('users', 'array-contains', user.email);
-  const [chatsSnapshot] = useCollection(userChatRef)
-
+  const [user] = useAuthState(auth);
+  const userChatRef = db
+    .collection("chats")
+    .where("users", "array-contains", user.email);
+  const [chatsSnapshot] = useCollection(userChatRef);
 
   const createChat = () => {
-    const input = prompt('Por favor, Digite o endereço de Email do usuario que deseja conversar.');
+    const input = prompt(
+      "Por favor, Digite o endereço de Email do usuario que deseja conversar."
+    );
 
     if (!input) return null;
 
-    if (EmailValidator.validate(input) && !chatAlreadyExists(input) && input !== user.email){
+    if (
+      EmailValidator.validate(input) &&
+      !chatAlreadyExists(input) &&
+      input !== user.email
+    ) {
       // we need to add the chat into DB 'chats' collection if it doesnt already exist and is valid
-      db.collection('chats').add({
+      db.collection("chats").add({
         users: [user.email, input],
-      }) 
-
+      });
     }
-  }
+  };
 
-  const chatAlreadyExists = (recipientEmail) => !!chatsSnapshot?.docs.find(
-    (chat) => chat.data().users.find((user) => user === recipientEmail)?.length > 0)
-  
+  const chatAlreadyExists = (recipientEmail) =>
+    !!chatsSnapshot?.docs.find(
+      (chat) =>
+        chat.data().users.find((user) => user === recipientEmail)?.length > 0
+    );
 
   return (
     <Container>
-        <Header>
-            <UserAvatar onClick={() => auth.signOut()} src={user.photoURL} />
-            <IconsContainer>
+      <Header>
+        <UserAvatar
+          style={{ height: 55, width: 55 }}
+          onClick={() => auth.signOut()}
+          src={user.photoURL}
+        />
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
 
-              <IconButton>
-                  <ChatIcon />
-              </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconsContainer>
+      </Header>
 
-              <IconButton>
-                <MoreVertIcon />
-              </IconButton >
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="Pesquise em conversas" />
+      </Search>
+      <SidebarButton onClick={createChat}>
+        Comece uma nova conversa
+      </SidebarButton>
 
-            </IconsContainer>
-        </Header>
-
-        <Search>
-          <SearchIcon />
-          <SearchInput placeholder="Pesquise em conversas"/>
-        </Search>
-        <SidebarButton onClick={createChat}>
-          Comece uma nova conversa
-        </SidebarButton>
-
-        {/* lists of chats */}
-        {chatsSnapshot?.docs.map((chat) => (
-          <Chat key={chat.id} id={chat.id} users={chat.data().users} />
-        ))}
+      {/* lists of chats */}
+      {chatsSnapshot?.docs.map((chat) => (
+        <Chat key={chat.id} id={chat.id} users={chat.data().users} />
+      ))}
     </Container>
-  )
+  );
 }
 
-const Container = styled.div`
-
-
-`;
+const Container = styled.div``;
 
 const Search = styled.div`
-
   display: flex;
   align-items: center;
   padding: 5px;
   border-radius: 2px;
-`
+`;
 
 const SidebarButton = styled(Button)`
   width: 100%;
 
-  &&&{
+  &&& {
     border-top: 1px solid whitesmoke;
     border-bottom: 1px solid whitesmoke;
   }
-`
+`;
 
 const SearchInput = styled.input`
-
   outline-width: 0;
   border: none;
   flex: 1;
-`
+`;
 
 const Header = styled.div`
   display: flex;
@@ -104,19 +108,16 @@ const Header = styled.div`
   z-index: 1;
   justify-content: space-between;
   align-items: center;
-  padding: 15px;
+  padding: 23px;
   border-bottom: 1px solid whitesmoke;
-`
+`;
 
 const UserAvatar = styled(Avatar)`
   cursor: pointer;
 
-  :hover{
+  :hover {
     opacity: 0.8;
   }
-
 `;
 
-const IconsContainer = styled.div`
-
-`;
+const IconsContainer = styled.div``;
--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,30 @@
-import { useAuthState } from 'react-firebase-hooks/auth'
+import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../firebase";
-import Login from './login';
-import Loading from '@/components/Loading';
-import { useEffect } from 'react';
-import firebase from 'firebase/compat/app';
+import Login from "./login";
+import Loading from "@/components/Loading";
+import { useEffect } from "react";
+import firebase from "firebase/compat/app";
 
-import '../styles/globals.css'
-
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-
   const [user, loading] = useAuthState(auth);
 
   useEffect(() => {
-    if(user){
-      db.collection('users').doc(user.uid).set({
-        email: user.email,
-        lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
-        photoURL: user.photoURL,
-      },
-      { merge: true }
+    if (user) {
+      db.collection("users").doc(user.uid).set(
+        {
+          email: user.email,
+          lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+          photoURL: user.photoURL,
+        },
+        { merge: true }
       );
     }
-  }, [user])
+  }, [user]);
 
+  if (loading) return <Loading />;
+  if (!user) return <Login />;
 
-  if (loading) return <Loading />
-  if (!user) return <Login />
-
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
